835c0c9d2be650cfac45037205fc1dbeb4555bed
rahimedu <edwin.duche82@gmail.com>
2022-09-30 01:03:34 -0500

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+
+  # PATCH /birds/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  #DELETE /plants
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      render json: []
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
